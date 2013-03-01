08c70a32c0c1d3cdb1dc25a79612a1d655c0ee3a
Warren Rexroad <wrexroad@gmail.com>
2013-03-01 10:05:06 -0800

    Ported in the rebin() and make_edges() routines from IDL. Still need to finish the peak511() routine.

--- src/edu/ucsc/barrel/cdf_gen/SpectrumExtract.java
+++ src/edu/ucsc/barrel/cdf_gen/SpectrumExtract.java
@@ -37,75 +37,246 @@ import org.apache.commons.math3.optim.nonlinear.vector.
           jacobian.LevenbergMarquardtOptimizer;
 
 public class SpectrumExtract {
-   
+
+   //create uncalibrated bin edges
+   private static double[][] edges_raw = {
+      {0, 75, 230, 350, 620},
+      {
+         42, 46, 50, 53, 57, 60, 64, 70, 78, 84, 92, 100, 
+         106 , 114, 120, 128, 140, 156, 168, 184, 200, 212, 228, 
+         240, 256, 280, 312, 336, 368, 400, 424, 456, 480, 512, 
+         560, 624, 672, 736, 800, 848, 912, 960, 1024, 1120, 
+         1248, 1344, 1472, 1600, 1696
+      },
+      {
+         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
+         16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 
+         30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 
+         44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 
+         58, 59, 60, 61, 62, 63, 64, 66, 68, 70, 72, 74, 76, 78, 
+         80, 82, 84, 86, 88, 90, 92, 94, 96, 98, 100, 102, 104, 106, 
+         108, 110, 112, 114, 116, 118, 120, 122, 124, 126, 128, 132, 
+         136, 140, 144, 148, 152, 156, 160, 164, 168, 172, 176, 
+         180, 184, 188, 192, 196, 200, 204, 208, 212, 216, 220, 
+         224, 228, 232, 236, 240, 244, 248, 252, 256, 264, 272, 
+         280, 288, 296, 304, 312, 320, 328, 336, 344, 352, 360, 
+         368, 376, 384, 392, 400, 408, 416, 424, 432, 440, 448, 
+         456, 464, 472, 480, 488, 496, 504, 512, 528, 544, 560, 
+         576, 592, 608, 624, 640, 656, 672, 688, 704, 720, 736, 
+         752, 768, 784, 800, 816, 832, 848, 864, 880, 896, 912, 
+         928, 944, 960, 976, 992, 1008, 1024, 1056, 1088, 1120, 
+         1152, 1184, 1216, 1248, 1280, 1312, 1344, 1376, 1408, 
+         1440, 1472, 1504, 1536, 1568, 1600, 1632, 1664, 1696, 
+         1728, 1760, 1792, 1824, 1856, 1888, 1920, 1952, 1984, 
+         2016, 2048, 2112, 2176, 2240, 2304, 2368, 2432, 2496, 
+         2560, 2624, 2688, 2752, 2816, 2880, 2944, 3008, 3072, 
+         3136, 3200, 3264, 3328, 3392, 3456, 3520, 3584, 3648, 
+         3712, 3776, 3840, 3904, 3968, 4032, 4096
+      }
+   };
+   private static double[] slow_bin_widths;
+
+   public SpectrumExtract(){
+      //calculate the nominal slow spectrum bin widths
+      slow_bin_widths =  new double[edges_raw[2].length - 1];     
+      for(int edge_i = 1; edge_i < edges_raw[2].length; edge_i++){
+         slow_bin_widths[edge_i - 1] = 
+            edges_raw[2][edge_i] - edges_raw[2][edge_i - 1];
+      }
+   }
+
    public static double[][] createBinEdges(
-      double xtal_temp, double dpu_temp, int bin_511
+      double xtal_temp, double dpu_temp, int peak511
    ){
-      //edge array index reference
-      final int FAST = 0;
-      final int MED = 1;
-      final int SLOW = 2;
-      
-      //nominal keV/bin
-      final double SCALE = 2.5; 
-      
-      //create uncalibrated bin edges
-      double[][] edges = {
-         {0, 75, 230, 350, 620},
-         {
-            42, 46, 50, 53, 57, 60, 64, 70, 78, 84, 92, 100, 
-            106 , 114, 120, 128, 140, 156, 168, 184, 200, 212, 228, 
-            240, 256, 280, 312, 336, 368, 400, 424, 456, 480, 512, 
-            560, 624, 672, 736, 800, 848, 912, 960, 1024, 1120, 
-            1248, 1344, 1472, 1600, 1696
-         },
-         {
-            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
-            16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 
-            30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 
-            44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 
-            58, 59, 60, 61, 62, 63, 64, 66, 68, 70, 72, 74, 76, 78, 
-            80, 82, 84, 86, 88, 90, 92, 94, 96, 98, 100, 102, 104, 106, 
-            108, 110, 112, 114, 116, 118, 120, 122, 124, 126, 128, 132, 
-            136, 140, 144, 148, 152, 156, 160, 164, 168, 172, 176, 
-            180, 184, 188, 192, 196, 200, 204, 208, 212, 216, 220, 
-            224, 228, 232, 236, 240, 244, 248, 252, 256, 264, 272, 
-            280, 288, 296, 304, 312, 320, 328, 336, 344, 352, 360, 
-            368, 376, 384, 392, 400, 408, 416, 424, 432, 440, 448, 
-            456, 464, 472, 480, 488, 496, 504, 512, 528, 544, 560, 
-            576, 592, 608, 624, 640, 656, 672, 688, 704, 720, 736, 
-            752, 768, 784, 800, 816, 832, 848, 864, 880, 896, 912, 
-            928, 944, 960, 976, 992, 1008, 1024, 1056, 1088, 1120, 
-            1152, 1184, 1216, 1248, 1280, 1312, 1344, 1376, 1408, 
-            1440, 1472, 1504, 1536, 1568, 1600, 1632, 1664, 1696, 
-            1728, 1760, 1792, 1824, 1856, 1888, 1920, 1952, 1984, 
-            2016, 2048, 2112, 2176, 2240, 2304, 2368, 2432, 2496, 
-            2560, 2624, 2688, 2752, 2816, 2880, 2944, 3008, 3072, 
-            3136, 3200, 3264, 3328, 3392, 3456, 3520, 3584, 3648, 
-            3712, 3776, 3840, 3904, 3968, 4032, 4096
+      double factor1, factor2, temp, scale;
+      double[][] edges_cal = edges_raw;
+
+      //quadratic function for crystal gain drift with temperature
+      factor1 = 1;
+      if(xtal_temp != 0){
+         factor1 += 
+            (Math.pow(1.4, -4) * xtal_temp - Math.pow(6.8, -3)) * xtal_temp;
+      }
+
+      //linear function for peak detect gain drift with temperature
+      factor2 = 1;
+      if(dpu_temp != 0){
+         factor2 += (23 - dpu_temp) * Math.pow(3.4, -4);
+      }
+
+      //set an overall scale factor to position 511keV line
+      //this also helps compensate incorrect temperature values
+      scale = 2.5; //nominal keV/bin
+      if(peak511 != 0){
+         scale = 511 * factor2 / factor1 / peak511 / 
+            (1 - 11.6 / (peak511 + 10.8) + Math.pow(9.1, -5) * peak511);
+      }
+
+      //apply corrections to energy bin edges
+      temp = scale * factor1 / factor2;
+      for(int set_i = 0; set_i < 3; set_i++){
+         for(int edge_i = 0; edge_i < edges_cal[set_i].length; edge_i++){
+            edges_cal[set_i][edge_i] = 
+               temp * (
+                  edges_cal[set_i][edge_i] * (
+                     1 - 11.6 / (edges_cal[set_i][edge_i] + 10.8) 
+                     + Math.pow(9.1, -5) * edges_cal[set_i][edge_i]
+                  )
+               );
          }
-      };
-      
-      //calculate scintillator correction factor
-      double xtal_factor  = 1 + (0.00014 * xtal_temp - 0.0068) * xtal_temp;
-      
-      //calulate dpu correction factor
-      double dpu_factor = 1 + (23 - dpu_temp) * 0.00034;
-      
-      //calculate the 511 scale factor
-      double scale =
-         511 * dpu_factor / xtal_factor / bin_511 /
-         (1.0 - 11.6 / (bin_511 + 10.8) + 9.1e-5 * bin_511);
-      
-      return edges;
+      }
+
+      return edges_cal;
    }
    
-   public static void rebin(){
+   public static float[] rebin(
+      int[] oldVals, int[] oldBins, double[] newBins, 
+      int n, int m, boolean flux
+   ){
 
+      if ((n < 2) || (m < 2)){
+         System.out.println(
+            "length(s) violation: SpectraExtract.rebin(): " + n + ", " + m
+         );
+         System.exit(1);
+      }
+     
+      float[] result = new float[m - 1];
+      int oldLo = oldBins[0];
+      int oldHi = oldBins[1];
+      double newLo = newBins[0];
+      double newHi = newBins[1];
+      int newIndex = 0;
+      int oldIndex = 0;
+      double total = 0;
+
+      while(true){
+         if (oldHi <= newLo){
+            oldIndex++;
+            if(oldIndex >= (n - 1)){return result;}
+            oldLo = oldHi;
+            oldHi = oldBins[oldIndex + 1];
+            continue;
+         }
+         if (newHi <= oldLo){
+            if(flux){
+               result[newIndex] = (float) (total / (newHi - newLo));
+            }else{
+               result[newIndex] = (float) (total / (oldHi - oldLo));
+            }
+            
+            total = 0;
+            newIndex++;
+            
+            if(newIndex >= (m - 1)){return result;}
+            
+            newLo = newHi;
+            newHi = newBins[newIndex + 1];
+
+            continue;
+         }
+
+         if (newHi < oldHi){
+            total += (newHi - Math.max(oldLo, newLo)) * oldVals[oldIndex];
+            if(flux){
+               result[newIndex] = (float) (total / (newHi-newLo));
+            }else{
+               result[newIndex] = (float) (total / (oldHi-oldLo));
+            }
+            total = 0;
+            newIndex++;
+            if(newIndex >= (m - 1)){return result;}
+            newLo = newHi;
+            newHi = newBins[newIndex + 1];
+            continue;
+         }else{
+            total += (oldHi - Math.max(oldLo,newLo)) * oldVals[oldIndex];
+            oldIndex++;
+            if (oldIndex >= (n - 1)){
+               if(flux){
+                  result[newIndex] = (float) (total / (newHi-newLo));
+               }
+               else{
+                  result[newIndex] = (float) (total / (oldHi-oldLo));
+               }
+               return result;
+            }
+            oldLo = oldHi;
+            oldHi = oldBins[oldIndex + 1];
+         }
+      }
    }
 
-   private static void find511(){
+   private static void find511(double[] slow, double[] err, int offset){
       GaussianFitter fitter = 
          new GaussianFitter(new LevenbergMarquardtOptimizer());
+
+/*
+x= [0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5, $
+    11.5, 12.5, 13.5, 14.5, 15.5, 16.5, 17.5, 18.5, 19.5, 20.5, $
+    21.5, 22.5, 23.5, 24.5, 25.5, 26.5, 27.5, 28.5, 29.5, 30.5, $
+    31.5, 32.5, 33.5, 34.5, 35.5, 36.5, 37.5, 38.5, 39.5, 40.5, $
+    41.5, 42.5, 43.5, 44.5, 45.5, 46.5, 47.5, 48.5, 49.5, 50.5, $
+    51.5, 52.5, 53.5, 54.5, 55.5, 56.5, 57.5, 58.5, 59.5, 60.5, $
+    61.5, 62.5, 63.5, 65, 67, 69, 71, 73, 75, 77, 79, 81, $
+    83, 85, 87, 89, 91, 93, 95, 97, 99, 101, 103, 105, $
+    107, 109, 111, 113, 115, 117, 119, 121, 123, 125, $
+    127, 130, 134, 138, 142, 146, 150, 154, 158, 162, $
+    166, 170, 174, 178, 182, 186, 190, 194, 198, 202, $
+    206, 210, 214, 218, 222, 226, 230, 234, 238, 242, $
+    246, 250, 254, 260, 268, 276, 284, 292, 300, 308, $
+    316, 324, 332, 340, 348, 356, 364, 372, 380, 388, $
+    396, 404, 412, 420, 428, 436, 444, 452, 460, 468, $
+    476, 484, 492, 500, 508, 520, 536, 552, 568, 584, $
+    600, 616, 632, 648, 664, 680, 696, 712, 728, 744, $
+    760, 776, 792, 808, 824, 840, 856, 872, 888, 904, $
+    920, 936, 952, 968, 984, 1000, 1016, 1040, 1072, 1104, $
+    1136, 1168, 1200, 1232, 1264, 1296, 1328, 1360, 1392, $
+    1424, 1456, 1488, 1520, 1552, 1584, 1616, 1648, 1680, $
+    1712, 1744, 1776, 1808, 1840, 1872, 1904, 1936, 1968, $
+    2000, 2032, 2080, 2144, 2208, 2272, 2336, 2400, 2464, $
+    2528, 2592, 2656, 2720, 2784, 2848, 2912, 2976, 3040, $
+    3104, 3168, 3232, 3296, 3360, 3424, 3488, 3552, 3616, $
+    3680, 3744, 3808, 3872, 3936, 4000, 4064] 
+
+; nominal range for 511 line
+  width=25
+  select=indgen(width)+offset
+  y = slow[select]/widths[select]
+  x = x[select]
+  err = err[select]/widths[select]
+  usersym,[-.5,.5,.5,-.5,-.5],[-.5,-.5,.5,.5,-.5],/fill
+
+; describe a line through endpoints of the selected range
+  y1 = y[0]
+  y2 = y[width-1]
+  x1 = x[0]
+  x2 = x[width-1]
+  m = (y2-y1)/(x2-x1)
+  b = y1 - m*x1
+
+;
+; find approximate peak location after subtracting linear bkgd
+;
+  peakregion = y - (m * x + b)
+  apex=max(peakregion)
+  higharea=where(peakregion gt 0.5*apex,cnt)
+  if (cnt lt 2) then return, -1
+  peaklocation = x[floor(median(higharea))]
+
+  guess=[1., peaklocation, 10., m, b]
+  yfit=curvefit(x,y,1./err^2,guess, $
+       chisq=chisq,sigma,function_name='mygauss',status=stat)
+  if (guess[2] gt 20 or guess[0] lt 0.2) then return, -1
+
+;  some diagnostics (might need to adjust above tests)
+;
+;  print,guess
+;  plot,x,y,psym=8,yrange=[0,3]
+;  oplot,x,yfit
+;  oploterr,x,y,err
+
+  return,guess[1]
+*/
    }
 }
