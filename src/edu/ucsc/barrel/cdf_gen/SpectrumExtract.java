bdbf2cf39b59d0b9d9859c9837a1a2c330e9043a
Warren Rexroad <wrexroad@gmail.com>
2013-03-05 06:23:59 -0800

    makeEdges() and rebin() have been ported in from Michael's IDL code. find511() has had some work done, but still needs a lot of testing.

--- src/edu/ucsc/barrel/cdf_gen/SpectrumExtract.java
+++ src/edu/ucsc/barrel/cdf_gen/SpectrumExtract.java
@@ -75,13 +75,20 @@ public class SpectrumExtract {
       }
    };
    private static double[] slow_bin_widths;
+   private static double[] slow_bin_midpoints;
 
    public SpectrumExtract(){
-      //calculate the nominal slow spectrum bin widths
+      //calculate the nominal slow spectrum bin widths and midpoints
       slow_bin_widths =  new double[edges_raw[2].length - 1];     
       for(int edge_i = 1; edge_i < edges_raw[2].length; edge_i++){
          slow_bin_widths[edge_i - 1] = 
             edges_raw[2][edge_i] - edges_raw[2][edge_i - 1];
+      }
+
+      slow_bin_midpoints = new double[slow_bin_widths.length];
+      for(int bin_i = 0; bin_i < slow_bin_widths.length; bin_i++){
+         slow_bin_midpoints[bin_i] = 
+            (slow_bin_widths[bin_i] / 2) + edges_raw[2][bin_i];
       }
    }
 
@@ -207,67 +214,75 @@ public class SpectrumExtract {
       }
    }
 
-   private static void find511(double[] slow, double[] err, int offset){
+   public static int find511(double[] slow, double[] err, int offset){
       GaussianFitter fitter = 
          new GaussianFitter(new LevenbergMarquardtOptimizer());
+      
+      //nominal range for 511 line
+      int width = 25;
+      
+      //create array that lists the range of indicies 
+      //in which we expect to see the 511 line
+      //get a set of points to fit
+      double[] y = new double[width];
+      double[] x = new double[width];
+      double[] errors = new double[width];
+      for(int pnt_i = 0; pnt_i < width; pnt_i++){
+         y[pnt_i] = slow[pnt_i + offset] / slow_bin_widths[pnt_i + offset];
+         x[pnt_i] = slow_bin_midpoints[pnt_i + offset];
+         errors[pnt_i] = err[pnt_i + offset] / slow_bin_widths[pnt_i + offset];
+      }
 
-/*
-x= [0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5, $
-    11.5, 12.5, 13.5, 14.5, 15.5, 16.5, 17.5, 18.5, 19.5, 20.5, $
-    21.5, 22.5, 23.5, 24.5, 25.5, 26.5, 27.5, 28.5, 29.5, 30.5, $
-    31.5, 32.5, 33.5, 34.5, 35.5, 36.5, 37.5, 38.5, 39.5, 40.5, $
-    41.5, 42.5, 43.5, 44.5, 45.5, 46.5, 47.5, 48.5, 49.5, 50.5, $
-    51.5, 52.5, 53.5, 54.5, 55.5, 56.5, 57.5, 58.5, 59.5, 60.5, $
-    61.5, 62.5, 63.5, 65, 67, 69, 71, 73, 75, 77, 79, 81, $
-    83, 85, 87, 89, 91, 93, 95, 97, 99, 101, 103, 105, $
-    107, 109, 111, 113, 115, 117, 119, 121, 123, 125, $
-    127, 130, 134, 138, 142, 146, 150, 154, 158, 162, $
-    166, 170, 174, 178, 182, 186, 190, 194, 198, 202, $
-    206, 210, 214, 218, 222, 226, 230, 234, 238, 242, $
-    246, 250, 254, 260, 268, 276, 284, 292, 300, 308, $
-    316, 324, 332, 340, 348, 356, 364, 372, 380, 388, $
-    396, 404, 412, 420, 428, 436, 444, 452, 460, 468, $
-    476, 484, 492, 500, 508, 520, 536, 552, 568, 584, $
-    600, 616, 632, 648, 664, 680, 696, 712, 728, 744, $
-    760, 776, 792, 808, 824, 840, 856, 872, 888, 904, $
-    920, 936, 952, 968, 984, 1000, 1016, 1040, 1072, 1104, $
-    1136, 1168, 1200, 1232, 1264, 1296, 1328, 1360, 1392, $
-    1424, 1456, 1488, 1520, 1552, 1584, 1616, 1648, 1680, $
-    1712, 1744, 1776, 1808, 1840, 1872, 1904, 1936, 1968, $
-    2000, 2032, 2080, 2144, 2208, 2272, 2336, 2400, 2464, $
-    2528, 2592, 2656, 2720, 2784, 2848, 2912, 2976, 3040, $
-    3104, 3168, 3232, 3296, 3360, 3424, 3488, 3552, 3616, $
-    3680, 3744, 3808, 3872, 3936, 4000, 4064] 
+      //describe a line through endpoints of the selected range
+      double y1 = y[0];
+      double y2 = y[width - 1];
+      double x1 = x[0];
+      double x2 = x[width - 1];
+      double m = (y2 - y1) / (x2 - x1);
+      double b = y1 - m * x1;
 
-; nominal range for 511 line
-  width=25
-  select=indgen(width)+offset
-  y = slow[select]/widths[select]
-  x = x[select]
-  err = err[select]/widths[select]
-  usersym,[-.5,.5,.5,-.5,-.5],[-.5,-.5,.5,.5,-.5],/fill
+      //find approximate peak location after subtracting linear bkgd
+      double[] peakregion = new double[width];
+      for(int pnt_i = 0; pnt_i < width; pnt_i++){
+         peakregion[pnt_i] = y[pnt_i] - (m * x[pnt_i] + b);
+      }
+      
+      //find the max of the peak region
+      double apex = 0;
+      for(int pnt_i = 0; pnt_i < width; pnt_i++){
+         if(peakregion[pnt_i] > apex){apex = y[pnt_i];}
+      }
 
-; describe a line through endpoints of the selected range
-  y1 = y[0]
-  y2 = y[width-1]
-  x1 = x[0]
-  x2 = x[width-1]
-  m = (y2-y1)/(x2-x1)
-  b = y1 - m*x1
 
-;
-; find approximate peak location after subtracting linear bkgd
-;
-  peakregion = y - (m * x + b)
-  apex=max(peakregion)
-  higharea=where(peakregion gt 0.5*apex,cnt)
-  if (cnt lt 2) then return, -1
-  peaklocation = x[floor(median(higharea))]
+      //find the location of all points near the peak
+      //use high_i to keep track of the last element in the array
+      int[] higharea = new int[width];
+      int high_i = 0;
+      for(int peak_i = 0; peak_i < width; peak_i++){
+         if(peakregion[peak_i] > (0.5 * apex)){
+            //record the index of peak region with the high point
+            higharea[high_i] = peak_i;
+            high_i++;
+         }
+      }
+      if(high_i < 2){ return -1;}
 
-  guess=[1., peaklocation, 10., m, b]
-  yfit=curvefit(x,y,1./err^2,guess, $
-       chisq=chisq,sigma,function_name='mygauss',status=stat)
-  if (guess[2] gt 20 or guess[0] lt 0.2) then return, -1
+      //add all of the points in higharea to fitter, then do the fit
+      for(int pnt_i = 0; pnt_i < high_i; pnt_i++){
+         fitter.addObservedPoint(x[higharea[pnt_i]], y[higharea[pnt_i]]);
+      }
+      double[] fit_params = fitter.fit();
+System.out.println(fit_params[0] +" "+fit_params[0]+" "+fit_params[0]);
+/*     
+      //get a best guess of the peak location 
+      //taking the middle value of of higharea
+      int higharea_median = higharea[high_i / 2];
+      int peaklocation = (int) x[higharea_median];
+
+      guess = [1., peaklocation, 10., m, b];
+      yfit=curvefit(x,y,1./err^2,guess, $
+         chisq=chisq,sigma,function_name='mygauss',status=stat)
+      if (guess[2] gt 20 or guess[0] lt 0.2) then return, -1
 
 ;  some diagnostics (might need to adjust above tests)
 ;
@@ -277,6 +292,8 @@ x= [0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5, $
 ;  oploterr,x,y,err
 
   return,guess[1]
-*/
+  */
+
+     return 1;
    }
 }
