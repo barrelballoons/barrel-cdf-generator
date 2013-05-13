58a235f96f719b3df8b20f2ff15d8bb3c18629f1
Warren Rexroad <wrexroad@gmail.com>
2013-05-13 13:15:25 -0700

    Magnetic coordinates are generated and saved to an array, just not saved to the CDF yet.

--- src/edu/ucsc/barrel/cdf_gen/LevelTwo.java
+++ src/edu/ucsc/barrel/cdf_gen/LevelTwo.java
@@ -5,6 +5,7 @@ import gsfc.nssdc.cdf.CDFException;
 import gsfc.nssdc.cdf.util.CDFTT2000;
 import gsfc.nssdc.cdf.Variable;
 
+import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -112,63 +113,98 @@ public class LevelTwo{
    public void doGpsCdf(int first, int last, int date) throws CDFException{
       CDF cdf;
       Variable var;
+      Calendar d = Calendar.getInstance();
+      String line;
       int 
-         dayOfYear,
+         year, month, day, day_of_year,
          numOfRecs = last - first;
+      double
+         sec_of_day = 0;
+      String[] 
+         mag_coords = new String[3];
       float[] 
          lat = new float[numOfRecs], 
          lon = new float[numOfRecs], 
-         alt = new float[numOfRecs];
-         mlt = new float[numOfRecs];
+         alt = new float[numOfRecs],
+         mlt = new float[numOfRecs],
          l = new float[numOfRecs];
       int[] 
          ms = new int[numOfRecs],
          frameGroup = new int[numOfRecs],
          q = new int[numOfRecs]; 
-      long[] epoch = new long[numOfRecs];
+      long[] 
+         epoch_parts = new long[9],
+         epoch = new long[numOfRecs];
 
       System.out.println("\nSaving GPS Level Two CDF...");
 
       //calculate the day of year
-      Date d = new Date
+      year = date / 10000;
+      month = (date - (year * 10000)) / 100;
+      day = date - (year * 10000) - (month * 100);
+      d.set(Calendar.YEAR, year + 2000);
+      d.set(Calendar.MONTH, month - 1);
+      d.set(Calendar.DAY_OF_MONTH, day);
+      day_of_year = d.get(Calendar.DAY_OF_YEAR);
+      System.out.println(day_of_year);
 
       //convert lat, lon, and alt values and select values for this date
       for(int rec_i = 0, data_i = first; data_i < last; rec_i++, data_i++){
-        //convert mm to km
-        alt[rec_i] = (float)data.gps_raw[0][data_i] / 1000000;
+         //convert mm to km
+         alt[rec_i] = (float)data.gps_raw[0][data_i] / 1000000;
 
-        //convert lat and lon to physical units
-        lat[rec_i] = (float)data.gps_raw[2][data_i];
-        lat[rec_i] *= 
-           Float.intBitsToFloat(Integer.valueOf("33B40000",16).intValue());
+         //convert lat and lon to physical units
+         lat[rec_i] = (float)data.gps_raw[2][data_i];
+         lat[rec_i] *= 
+            Float.intBitsToFloat(Integer.valueOf("33B40000",16).intValue());
 
-        lon[rec_i] = (float)data.gps_raw[3][data_i];
-        lon[rec_i] *= 
-           Float.intBitsToFloat(Integer.valueOf("33B40000",16).intValue());
+         lon[rec_i] = (float)data.gps_raw[3][data_i];
+         lon[rec_i] *= 
+            Float.intBitsToFloat(Integer.valueOf("33B40000", 16).intValue());
 
-        //save the values from the other variables
-        ms[rec_i] = data.gps_raw[1][data_i];
-        frameGroup[rec_i] = data.frame_mod4[data_i];
-        epoch[rec_i] = data.epoch_mod4[data_i];
-        q[rec_i] = data.gps_q[data_i];
+         //save the values from the other variables
+         ms[rec_i] = data.gps_raw[1][data_i];
+         frameGroup[rec_i] = data.frame_mod4[data_i];
+         epoch[rec_i] = data.epoch_mod4[data_i];
+         q[rec_i] = data.gps_q[data_i];
 
-        //get the magnetic field info for this location
-        try {
-           String command = 
-              "./print_l_mlt " + 
-              frameGroup[rec_i] + " " + alt[rec_i] + " " + lat[rec_i] + " " + 
-              lon[rec_i] + " 2013 " + "";
-           Process p = Runtime.getRuntime().exec(command);
-           BufferedReader input = 
-              new BufferedReader(new InputStreamReader(p.getInputStream()));
-           while ((String line = input.readLine()) != null) {
-              System.out.println(line + "\n");
-           }
-           input.close();
-        }
-        catch (Exception ex) {
-           ex.printStackTrace();
-        }
+         //make sure we have a complete gps record before generating mag coords
+         if((alt[rec_i] != 0) && (lat[rec_i] != 0) && (lon[rec_i] != 0)){
+            try{
+               //calculate the current time in seconds of day
+               epoch_parts = CDFTT2000.breakdown(epoch[rec_i]);
+               sec_of_day = 
+                  (epoch_parts[3] * 3600) + // hours
+                  (epoch_parts[4] * 60) + //minutes
+                  epoch_parts[5] + //seconds
+                  (epoch_parts[6] / 0.001) + //ms
+                  (epoch_parts[7] / 0.000001) + //us
+                  (epoch_parts[8] / 0.000000001); //ns
+
+               //get the magnetic field info for this location
+               String command = 
+                  "./print_l_mlt " + 
+                  frameGroup[rec_i] +" "+ 
+                  alt[rec_i] +" "+ lat[rec_i] +" "+ lon[rec_i] +" "+ 
+                  (year + 2000)  +" "+  day_of_year +" "+ sec_of_day;
+               Process p = Runtime.getRuntime().exec(command);
+               BufferedReader input = 
+                  new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+               //read and save the mag coords
+               line = input.readLine();
+               mag_coords = line.split("\\s+");
+               l[rec_i] = Float.parseFloat(mag_coords[1]);
+               mlt[rec_i] = Float.parseFloat(mag_coords[2]);
+
+               input.close();
+            }catch(Exception ex){
+               //something went wrong, so dont save any coords for this record
+               System.out.println("Could not generate mag coords:");
+               System.out.println(ex.getMessage());
+               continue;
+            }
+         }
       }
 
       //make sure there is a CDF file to open
