be3941240cac8dcd6ea672fd564157c78badaa4e
Warren Rexroad <wrexroad@gmail.com>
2013-09-20 16:25:30 -0700

    Ported in makeedges() routine, although it is still untested and has not been incorperated yet

--- src/edu/ucsc/barrel/cdf_gen/SpectrumExtract.java
+++ src/edu/ucsc/barrel/cdf_gen/SpectrumExtract.java
@@ -24,10 +24,14 @@ Description:
 
 package edu.ucsc.barrel.cdf_gen;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.ListIterator;
 import java.lang.ArrayIndexOutOfBoundsException;
+
 import org.apache.commons.math3.fitting.GaussianFitter;
 import org.apache.commons.math3.optim.nonlinear.vector.
           jacobian.LevenbergMarquardtOptimizer;
@@ -234,7 +238,8 @@ public class SpectrumExtract {
 
    DESC: invert bin(energy) relation to find energy(bin)
 
-   INPUT: start is an initial estimate of bin value(s)
+   INPUT_ARGS: 
+          start is an initial estimate of bin value(s)
           f is a temperature/detector dependent constant
 
    OUTPUT: returns an object of the same type and dimension
@@ -260,35 +265,35 @@ public class SpectrumExtract {
    NOTES: binvert() should be used only by makedges().
           Ported from Michael McCarthy's original IDL code
 */
-   private double[] binvert(double[] start, double f){
+   private static float[] binvert(float[] start, float f){
       int 
          size = start.length,
          bad_vals = 0;
 
-      double[] iter1 = new double[size];
-      double[] iter2 = new double[size];
+      float[] iter1 = new float[size];
+      float[] iter2 = new float[size];
      
 
       //first iteration of Newton-Raphson  
       for(int i = 0; i < size; i++){
          if(start[i] < 0){
-            iter1[i] = Double.NaN;
+            iter1[i] = Float.NaN;
          }else{
             iter1[i] = 
                (start[i] + f * start[i]) / 
-               (1 + f * (1 + Math.log(start[i])));
+               (1.0f + f * (1.0f + (float)Math.log(start[i])));
          }
       }
 
       //second iteration of Newton-Raphson  
       for(int i = 0; i < size; i++){
-         if(Double.isNaN(iter1[i])){
+         if(Float.isNaN(iter1[i])){
             bad_vals++;
          }else{
             iter2[i] = 
                (iter1[i] + f * iter1[i]) / 
-               (1.0 + f * (1.0 + Math.log(iter1[i])));
-            if(Double.isInfinite(iter2[i])){
+               (1.0f + f * (1.0f + (float)Math.log(iter1[i])));
+            if(Float.isInfinite(iter2[i])){
                bad_vals++;
             }
          }
@@ -297,7 +302,7 @@ public class SpectrumExtract {
       //turn bad values into negatives ascending to zero
       if(bad_vals > 0){
          for(int i = 0; i < size; i++){
-            if(Double.isNaN(iter2[i]) || Double.isInfinite(iter2[i])){
+            if(Float.isNaN(iter2[i]) || Float.isInfinite(iter2[i])){
                bad_vals--;
                iter2[i] = 0 - bad_vals;
             }
@@ -306,10 +311,170 @@ public class SpectrumExtract {
 
       return iter2;
    }
+   //overload binvert to accept scalar
+   private static float binvert(float start, float f){
+      return (binvert(new float[] {start}, f))[0];
+   }
    
+/*
+   NAME: makeedges()
+   DESC: construct energy edges for spectrum products
 
-   public double[] makeedges(){
-      return new double[] {};
+   INPUT_ARGS: 
+        spec_i        indicates spectrum for which the edges are to be created
+                      (0=slo, 1=med, 2=fst)
+        payload      2 char identifier (e.g., '1S')
+        dpu_temp      dpu temperature in C
+        xtal_temp     scintillator temperature in C
+        peak511       sspc location for 511keV; 200 nominal; 0<peak511<4095 
+
+   INPUT_FILES: energy.cal
+
+   OUTPUT: An array containing 1 of the 3 lists of bin edges
+        slo is a float[257]
+        med is a float[49]
+        fst is a float[5]
+        For these 3 lists, each pair of edges brackets one of the
+          256/48/4 spectrum bins. Energy units are keV.
+        Energy values below 0 are forced to 0, so it is
+        possible for multiple low-end slo bins to have 0
+        energy. Counts in these bins should be ignored.
+
+   METHOD: invert an empirical model of bin(Energy). The model
+        is bin(E) = k*(offset + gain*E + nonlin*E*Log(E))
+        where offset, gain, and nonlin are linear functions
+        of DPU temperature, and k is a quadratic function
+        of crystal temperature. The dpu
+        temperature-dependent linear functions use coefficients
+        extracted from thermal chamber data, and are
+        retrieved via a function call. Because the coupling
+        between scintillator and crystal can change with air
+        pressure, the peak511 parameter should be used to
+        improve the model for flight data.
+
+        default values for missing options are probably not
+        sufficiently accurate
+
+
+   CALLS: binvert(start,f) inverts bin(energy) function;
+                needs a start value and factor f
+
+   NOTES: Ported from Michael McCarthy's original IDL code
+*/
+   public float[] makeedges(
+      int spec_i, final String payload, 
+      float xtal_temp, float dpu_temp, float peak511
+   ){
+      //initialize the uncalibrated bin edges
+      int[][] uncal_edges = {
+         {0, 75, 230, 350, 620},
+         {42, 46, 50, 53, 57, 60, 64, 70, 78, 84, 92, 100, 
+         106, 114, 120, 128, 140, 156, 168, 184, 200, 212, 228, 
+         240, 256, 280, 312, 336, 368, 400, 424, 456, 480, 512, 
+         560, 624, 672, 736, 800, 848, 912, 960, 1024, 1120, 
+         1248, 1344, 1472, 1600, 1696},
+         {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
+         16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 
+         30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 
+         44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 
+         58, 59, 60, 61, 62, 63, 64, 66, 68, 70, 72, 74, 76, 78, 
+         80, 82, 84, 86, 88, 90, 92, 94, 96, 98, 100, 102, 104, 106, 
+         108, 110, 112, 114, 116, 118, 120, 122, 124, 126, 128, 132, 
+         136, 140, 144, 148, 152, 156, 160, 164, 168, 172, 176, 
+         180, 184, 188, 192, 196, 200, 204, 208, 212, 216, 220, 
+         224, 228, 232, 236, 240, 244, 248, 252, 256, 264, 272, 
+         280, 288, 296, 304, 312, 320, 328, 336, 344, 352, 360, 
+         368, 376, 384, 392, 400, 408, 416, 424, 432, 440, 448, 
+         456, 464, 472, 480, 488, 496, 504, 512, 528, 544, 560, 
+         576, 592, 608, 624, 640, 656, 672, 688, 704, 720, 736, 
+         752, 768, 784, 800, 816, 832, 848, 864, 880, 896, 912, 
+         928, 944, 960, 976, 992, 1008, 1024, 1056, 1088, 1120, 
+         1152, 1184, 1216, 1248, 1280, 1312, 1344, 1376, 1408, 
+         1440, 1472, 1504, 1536, 1568, 1600, 1632, 1664, 1696, 
+         1728, 1760, 1792, 1824, 1856, 1888, 1920, 1952, 1984, 
+         2016, 2048, 2112, 2176, 2240, 2304, 2368, 2432, 2496, 
+         2560, 2624, 2688, 2752, 2816, 2880, 2944, 3008, 3072, 
+         3136, 3200, 3264, 3328, 3392, 3456, 3520, 3584, 3648, 
+         3712, 3776, 3840, 3904, 3968, 4032, 4096}
+      };
+
+      //initialize array for calibrated edges
+      float[] edges_out = new float[uncal_edges[spec_i].length];
+
+      //get dpu coefficients from calibration file
+      float[][] dpu_coeffs = {{-5f, -0.1f}, {-0.5f, -0.001f}, {-0.1f, 0.0001f}};
+      try{
+         File energy_cal = new File("energy.cal");
+         if(energy_cal.exists()){
+            FileReader fr = new FileReader(energy_cal);
+            BufferedReader br = new BufferedReader(fr);
+            
+            String line;
+            String[] line_parts;
+
+            while((line = br.readLine()) != null){
+               //split off comments
+               line_parts = line.split(";");
+
+               //split the payload ID from the rest
+               line_parts = line_parts[0].split(":");
+               line_parts[0] = line_parts[0].trim();
+               if(!payload.equals(line_parts[0])){continue;}
+
+               //we have the right payload, check for the right number of values 
+               line_parts[1] = line_parts[1].trim();
+               line_parts = line_parts[1].split(",");
+               if(line_parts.length != 6){continue;}
+
+               //the correct number of values were in the file
+               //overwrite defaults
+               dpu_coeffs = new float[][] {
+                  {Float.valueOf(line_parts[0]), Float.valueOf(line_parts[1])},
+                  {Float.valueOf(line_parts[2]), Float.valueOf(line_parts[3])},
+                  {Float.valueOf(line_parts[4]), Float.valueOf(line_parts[5])}
+               };
+            }
+         }
+      }catch(IOException ex){
+         System.out.println("Can not find energy calibration file:");
+         System.out.println(ex.getMessage());
+
+         for(int i = 0; i < uncal_edges[spec_i].length; i++){
+            edges_out[i] = uncal_edges[spec_i][i];
+         }
+         return edges_out;
+      }
+      //set model parameters 
+      float xtal_compensate = 
+         1.022f - 1.0574e-4f * (float)Math.pow(xtal_temp - 10.7f, 2);
+      float[] dpu_compensate = new float[]{
+         ((dpu_coeffs[0][0]) + (dpu_temp * dpu_coeffs[0][1])),
+         ((dpu_coeffs[1][0]) + (dpu_temp * dpu_coeffs[1][1])),
+         ((dpu_coeffs[2][0]) + (dpu_temp * dpu_coeffs[2][1]))
+      };
+      float factor = dpu_compensate[2] / dpu_compensate[1];
+      
+      //calculate a correction from 511keV location
+      float fac511 = 1.0f;
+      if(!Float.isNaN(peak511)){
+         float start = 
+            (peak511 / xtal_compensate - dpu_compensate[0]) / dpu_compensate[1];
+         fac511 = 511.0f / binvert(start,factor);
+      }
+
+      //calculate energies for the desired spectral product
+      float[] start = new float[uncal_edges[spec_i].length];
+      float offset = (dpu_compensate[0] / dpu_compensate[1]);
+      for(int i = 0; i < start.length; i++){
+         start[i] = (uncal_edges[spec_i][i] / xtal_compensate) - offset;
+      }
+
+      edges_out = binvert(start, factor);
+      for(int i = 0; i < edges_out.length; i++){
+         edges_out[i] *= fac511;
+      }
+
+      return edges_out;
    }
 
 
